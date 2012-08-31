cb6cfa3d09da1ad6d43b39b7723b50696ec2d351
Julien Eluard <julien.eluard@gmail.com>
2012-08-30 23:25:19 -0300

    Added more deps and production profile.

--- project.clj
+++ project.clj
@@ -1,7 +1,19 @@
+;;heroku config:add  JVM_OPTS
+
 (defproject renderit "0.0.1"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [ring/ring-core "1.1.1"]
                  [ring/ring-jetty-adapter "1.1.1"]
                  [compojure "1.1.1"]
                  [ring-json-response "0.2.0"]
-                 [tentacles "0.2.0-beta1"]])
+                 [net.sourceforge.plantuml/plantuml "7933"]
+                 [tentacles "0.2.0-beta1"]
+                 [enlive "1.0.1"]]
+  :profiles {
+    :production {
+      :mirrors {
+        #"central|clojars" "http://s3pository.herokuapp.com/clojure"
+      }
+    }
+  }
+  :min-lein-version "2.0.0")
