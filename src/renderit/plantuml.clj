634e355522d76560e383840ae2b4daa000b6bca5
Julien Eluard <julien.eluard@gmail.com>
2012-11-24 22:08:11 -0300

    Use with-open to cleanup resources.

--- src/renderit/plantuml.clj
+++ src/renderit/plantuml.clj
@@ -1,20 +1,24 @@
 (ns renderit.plantuml
-  (:import net.sourceforge.plantuml.SourceStringReader)
-  (:import net.sourceforge.plantuml.FileFormat)
-  (:import net.sourceforge.plantuml.FileFormatOption)
-  (:import java.io.ByteArrayOutputStream))
+  (:import (net.sourceforge.plantuml FileFormat FileFormatOption SourceStringReader))
+  (import java.io.ByteArrayOutputStream)
+  (:use clojure.java.io))
 
 (defn type-from-extension [extension]
-  ""
+  "plantuml FileFormat from file extension"
   (case extension
     "png" FileFormat/PNG
     "svg" FileFormat/SVG))
 
+(defn supported?
+  [extension]
+  (or (= extension "png")
+      (= extension "svg")))
+
 (defn render [source extension]
-  (let [reader (SourceStringReader. source)
-        outputStream (ByteArrayOutputStream.)]
-    (.generateImage reader outputStream (FileFormatOption. (type-from-extension extension)))
-    (.toByteArray outputStream)))
+  (let [reader (SourceStringReader. source)]
+    (with-open [outputStream (ByteArrayOutputStream.)]
+      (.generateImage reader outputStream (FileFormatOption. (type-from-extension extension)))
+      (.toByteArray outputStream))))
 
 (defn render-stream [source extension]
   (java.io.ByteArrayInputStream. (render source extension)))
