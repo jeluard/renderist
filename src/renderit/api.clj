b5729c3218cbe0ad6c0d4749f11453423e72a877
Julien Eluard <julien.eluard@gmail.com>
2012-11-24 22:09:11 -0300

    Moved api in a separate ns.

---
+++ src/renderit/api.clj
@@ -0,0 +1,32 @@
+(ns renderit.api
+  (:require [compojure.core :as c]
+            [renderit.gist :as g]
+            [renderit.plantuml :as p]
+            [ring.util.response :as r])
+  (:import java.util.Locale))
+
+(defn mime-from-extension
+  [extension]
+  (if (= extension "png") "image/png" "image/svg+xml"))
+
+(defn image-response
+  [file extension]
+  (-> (r/response (p/render-stream file extension))
+      (r/content-type (mime-from-extension extension))))
+
+(c/defroutes routes
+  (c/GET "/:id/:name.:extension" [id name extension :as {headers :headers}]
+    (if-not (p/supported? extension)
+      (-> (r/response (str "Unsupported extension " extension))
+          (r/status 415))
+      (let [gist (g/get-gist id)]
+        (if (= (:status gist) 404)
+          (r/not-found (format "Non existing gist %s" id))
+          (let [candidates (g/matching-files(g/list-files gist) name)]
+            (if (empty? candidates)
+              (r/not-found (format "Cannot find matching file for %s" name))
+              (if (not= (count candidates) 1)
+                (r/not-found (format "Too many candidates: %s" (apply str candidates)))
+                (if-let [file (g/extract-file gist (first candidates))]
+                  (image-response file extension)
+                  (r/not-found (format "No file %s in gist %s" name id)))))))))))
--- src/renderit/web.clj
+++ src/renderit/web.clj
@@ -5,26 +5,46 @@
           [ring.util.codec :only (base64-encode)]
           [ring.util.response :only (content-type not-found response status)]
           [compojure.core]
-          [net.cgrand.enlive-html]
-          [clojure.java.io :only (resource)]
-          [markdown :only (md-to-html-string)]
-          [clj-time.format :only (parse unparse formatter with-locale)])
-    (:require [compojure.route :as route])
+          [net.cgrand.enlive-html])
+    (:require [clj-time.format :as f]
+              [clojure.java.io :as io]
+              [compojure.route :as route]
+              [markdown :as md]
+              [renderit.api :as api])
     (:import java.util.Locale))
 
 (deftemplate index "public/index.html" [string]
   [:div#content] (content string))
 
-(def format (with-locale (formatter "MMM dd, yyyy") Locale/US));default to UTC time zone
+(def formatter (f/with-locale (f/formatter "MMM dd, yyyy") Locale/US)) ;default to UTC time zone
 
-(defsnippet gist-snippet "public/templates/diagram.html" [:body :> any-node] [{:keys [id description] date :created_at {author :login} :user} readme files]
+(comment defsnippet gist "public/templates/diagram.html" [:section]
+  []
+  [:#id] (set-attr :id (:filename (val file))))
+
+(comment defsnippet footer "footer.html" [:.footer]
+  [message]
+  [:.footer] (content message))
+;;(footer "hello") => ({:tag :div, :attrs {:class "footer"}, :content ("hello")})
+
+(comment deftemplate friends-list "friends.html"
+  [username friends]
+  [:.usrname] (content username)
+  [:ul.friends :li] (clone-for [f friends]
+                      (do-> (content f))
+                            (add-class "test"))
+  [:body] (append (footer (str "Goodbye, " username))))
+;;use directly as ring :body
+
+(defsnippet gist-snippet "public/templates/diagram.html" 
+  [:body :> any-node] [{:keys [id description] date :created_at {author :login} :user} readme files]
   [:span (nth-child 1)] (set-attr :href (str "/" author))
   [:span (nth-child 1)] (content author)
   [:span (nth-child 2)] (set-attr :href (str "https://gist.github.com/" id))
   [:span (nth-child 2)] (content (str "#" id))
   [:h1] (content description)
-  [:aside#date] (content (unparse format (parse date)))
-  [:pre#readme] (html-content (md-to-html-string readme))
+  [:aside#date] (content (f/unparse formatter (f/parse date)))
+  [:pre#readme] (html-content (md/md-to-html-string readme))
   [:section] (clone-for [file files]
                [:section#id] (set-attr :id (:filename (val file)))
                [:h2 :a] (content (:filename (val file)))
@@ -34,48 +54,28 @@
                [:code] (content (str "&lt;img alt=\"" (:filename (val file)) "\" src=\"http://renderit.herokuapp.com/api/" id "/" (:filename (val file)) ".png")))
                )
 
-(defn root-html [latest]
-  (apply str(index latest)))
+(defn load-page
+  [page]
+  (slurp (io/resource (str "public/" page)) :encoding "UTF-8"))
 
-(def page-404
-  (slurp (resource "public/404.html") :encoding "UTF-8"))
+(def page-index (load-page "index.html"))
 
-(def page-author
-  (slurp (resource "public/author.html") :encoding "UTF-8"))
+(def page-author (load-page "author.html"))
 
-(defn gist-page [id]
+(def page-404 (load-page "404.html"))
+
+(defn page-gist [id]
   (if-let [gist (get-gist id)]
     (apply str(index (gist-snippet gist (extract-file gist "Readme.md") (:files gist))))
     page-404))
 
-(defroutes api-routes
-  (GET "/:id/:name.:extension" [id name extension :as {headers :headers}] 
-    (if-not (or (= extension "png") (= extension "svg")) 
-      (-> (response (str "Unknow extension " extension))
-          (status 415))
-      (let [gist (get-gist id)]
-        (if (= (:status gist) 404) (not-found (format "Non existing gist %s" id))
-          (let [candidates (matching-files(list-files gist) name)]
-            (if (empty? candidates) (not-found (format "Cannot find matching file for %s" name))
-              (if (not= (count candidates) 1) (not-found (format "Too many candidates: %s" (apply str candidates)))
-                (if-let [file (extract-file gist (first candidates))]
-                  (->
-                    (response (render-stream file extension))
-                    (content-type (if (= extension "png") "image/png" "image/svg+xml")))
-                  (not-found (format "No file %s in gist %s" name id))
-      )))))))))
-
-(def latest [{:name "Name 1" :url "http://gists.github.com/12334"}
-             {:name "Name 2" :url "http://gists.github.com/123345"}])
-
 (defroutes all-routes
-  (context "/api" [] api-routes)
-  (GET "/" [] (root-html latest))
-  (GET "/:id" [id] (gist-page id))
+  (context "/api" [] api/routes)
+  (GET "/" [] page-index)
+  (GET "/:id" [id] (page-gist id))
   (route/resources "/")
   (route/not-found page-404))
 
 (defn -main [port]
   (System/setProperty "java.awt.headless" "true")
-  (serve all-routes {:port (Integer. port)})
-)
+  (serve all-routes {:port (Integer. port)}))
