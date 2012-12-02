a793c00ba025b71b4b67f197d6369836362f0cd8
Julien Eluard <julien.eluard@gmail.com>
2012-12-02 17:27:08 -0300

    Added GPL license.

--- project.clj
+++ project.clj
@@ -1,5 +1,18 @@
-;;heroku config:add  JVM_OPTS
-
+;; Copyright 2012 Julien Eluard
+;;
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+;;
 (defproject renderist "0.0.1"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/core.memoize "0.5.2"]
@@ -13,10 +26,10 @@
                  [tentacles "0.2.0-beta1"]
                  [enlive "1.0.1"]
                  [ring-mock "0.1.3"]]
+  :license {:name "GPL - v 3.0"
+            :url "http://www.gnu.org/licenses/gpl-3.0.en.html"
+            :distribution :repo}
   :profiles {
-    :dev {
-      :dependencies [[clj-ns-browser "1.3.0"]]
-    }
     :production {
       :mirrors {
         #"central|clojars" "http://s3pository.herokuapp.com/clojure"
